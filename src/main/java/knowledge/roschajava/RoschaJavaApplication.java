15390bf8754eea800bc2a88dd1d8e34008131551
RostikGlob <rostyslav.chabria@globallogic.com>
2022-06-30 13:18:00 +0300

    Initial commit

---
+++ src/main/java/knowledge/roschajava/RoschaJavaApplication.java
@@ -0,0 +1,13 @@
+package knowledge.roschajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoschaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoschaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/knowledge/roschajava/RoschaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package knowledge.roschajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoschaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
